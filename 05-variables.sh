1c9e9ff12dbeb870a0e074ceb033464a33accacd
saiteja <saiteja093@gmail.com>
2026-07-22 14:37:33 +0530

    test 1

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "please enter your user_name :: "
-read ueser_name 
+read user_name 
 
 echo "your user_name is mentioned bellow $user_name"
+
+echo "please enter your password :: "
+read -s password
+
+echo "your password = $password"
