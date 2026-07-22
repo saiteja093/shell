a57780cf27c450c2389a34600f3099efbbb57f63
saiteja <saiteja093@gmail.com>
2026-07-22 14:26:18 +0530

    testing

---
+++ 02-conversition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "sainath:: hi prashanth how are you ?"
+echo "prashanth:: hi sainath i am fine how are you doing"
+echo "sainath:: fine what did you bring for dinner"
+echo "prashanth:: dhal and rice sainath shall we go for break"
+echo "sainath:: ok call sai and other some teameats will go togeather"
+echo "prashanth:: wait for 5 minutes sainath"
+echo "sainath:: prashanth you said we go for break and again you are working sai prashanth will
+say that we go for break and he was not comming"
+echo "sai:: yes sainath prashanth will say likethat only but he was not come early"
