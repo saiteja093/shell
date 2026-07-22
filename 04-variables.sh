52db06b4c27b62b98f6658209141a9497ea049c8
saiteja <saiteja093@gmail.com>
2026-07-22 14:07:01 +0530

    added person 3

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,6 +4,7 @@
 # no spaces between name and =
 person1=$1
 person2=$2
+person3=$3
 
 echo "$person1:: hi $person2 how are you ?"
 echo "$person2:: hi $person1 i am fine how are you doing"
@@ -13,4 +14,4 @@ echo "$person1:: ok call sai and other some teameats will go togeather"
 echo "$person2:: wait for 5 minutes $person1"
 echo "$person1:: $person2 you said we go for break and again you are working sai $person2 will
 say that we go for break and he was not comming"
-echo "sai:: yes $person1 $person2 will say likethat only but he was not come early"
+echo "$person3:: yes $person1 $person2 will say likethat only but he was not come early"
