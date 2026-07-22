d078d4e5ac2b25e2f360f8e6c36d147176d985c2
saiteja <saiteja093@gmail.com>
2026-07-22 13:57:46 +0530

    added variables-03

---
+++ 03-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# no spaces between name and =
+person1=sainath
+person2=prashanth
+
+echo "$person1:: hi $person2 how are you ?"
+echo "$person2:: hi $person1 i am fine how are you doing"
+echo "$person1:: fine what did you bring for dinner"
+echo "$person2:: dhal and rice $person1 shall we go for break"
+echo "$person1:: ok call sai and other some teameats will go togeather"
+echo "$person2:: wait for 5 minutes $person1"
+echo "$person1:: $person2 you said we go for break and again you are working sai $person2 will
+say that we go for break and he was not comming"
+echo "sai:: yes $person1 $person2 will say likethat only but he was not come early"
