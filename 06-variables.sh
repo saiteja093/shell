892ac08885ccc60a5cb723fbeabebefc7c5255fb
saiteja <saiteja093@gmail.com>
2026-07-23 05:46:22 +0530

    testing time_stamp

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-start_time=$(date)
+start_time=$(date +%s)
 echo "script executed at :: $timestamp"
 
 sleep 10
 
-end_time=$(date)
+end_time=$(date +%s)
 total_time=$(($end_time-$start_time))
 
 echo"script executed in :: $total_time"
