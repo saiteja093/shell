74786b9256f42226b398c9b8c07aed33a12df06c
saiteja <saiteja093@gmail.com>
2026-07-23 06:09:23 +0530

    testing time_stamp

---
+++ 07-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#### special variables ####
+echo "all args passed to script :: $@"
+echo "number of vars passed to script :: $#"
+echo "script name :: $0"
+echo "present directory :: $pWD"
+echo "who is running :: $USER"
+echo "home directory of current user :: $HOME"
+echo "pid of this script :: $$"
+sleep 100 &
+echo "pid of recently executed background process :: $!"
+echo "all args passed to script :: $*"
