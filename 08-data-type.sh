c815ab4e10e829f790f8ac2e5f8ae8ab12743433
saiteja <saiteja093@gmail.com>
2026-07-26 13:58:41 +0530

    daty types

--- 08-data-type.sh
+++ 08-data-type.sh
@@ -11,4 +11,4 @@ echo "sum is : $sum"
 # array
 fruits=("apple" "banana" "mango")
 
-echo "fruits are :: ${fruits[2]}" "{fruits[1]}"
+echo "fruits are :: ${fruits[2]}" echo "fruits are :: ${fruits[0]}" 
