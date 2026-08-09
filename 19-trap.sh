4c57ff16e7cb9d67e7e5df41ace6507742e7a06a
saiteja <saiteja093@gmail.com>
2026-08-09 12:38:45 +0530

    trap

--- 19-trap.sh
+++ 19-trap.sh
@@ -2,7 +2,7 @@
 
 set -e # ERR
 
-trap 'echo "there is an error in $LINENO, command: $BASH_COMMAND"' ERR
+trap 'echo "there is an error in $lineno, command: $bash_command"' ERR
 
 echo "hellow world"
 echo "i am learning shell"
