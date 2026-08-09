5c23035f78265551dd2a9fb1b76c2d8a5f7c1d7d
saiteja <saiteja093@gmail.com>
2026-08-09 08:50:45 +0530

    adding logs

--- 12-logs.sh
+++ 12-logs.sh
@@ -5,7 +5,7 @@ logs_folder="/var/log/shell"
 logs_file="/var/log/shell/$0.log"
 
 if [ $userid -ne 0 ]; then
-   echo "please run this script with root user access"
+   echo "please run this script with root user access" | tee -a $logs_file
    exit 1
 fi  
 mkdir -p $logs_folder
@@ -13,10 +13,10 @@ mkdir -p $logs_folder
 validate()
 {
     if [ $1 -ne 0 ]; then
-       echo "$2 ..... failure"
+       echo "$2 ..... failure" | tee -a $logs_file
        exit 1
    else
-       echo "$2 ......success"
+       echo "$2 ......success" | tee -a $logs_file
     fi        
 }
    
