91d8ba73797ea307b2405900240cea88dd7b8c8e
saiteja <saiteja093@gmail.com>
2026-08-09 10:20:26 +0530

    loops ex

---
+++ 15-loops.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# sudo sh 14-loops.sh nginx mysql nodejs
+
+userid=$(id -u)
+logs_folder="/var/log/shell"
+logs_file="/var/log/shell/$0.log"
+
+if [ $userid -ne 0 ]; then
+   echo "please run this script with root user access" | tee -a $logs_file
+   exit 1
+fi  
+mkdir -p $logs_folder
+#### by default shell will not execute, only exectue when it will call
+validate()
+{
+    if [ $1 -ne 0 ]; then
+       echo "$2 ..... failure" | tee -a $logs_file
+       exit 1
+   else
+       echo "$2 ......success" | tee -a $logs_file
+    fi        
+}
+   
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+  dnf list installed $package 
+  if [ $? -ne 0 ]; then 
+     echo "$package not installed, installing now"
+     dnf install $package -y &>> $logs_file
+     validate $? "package installation"
+    else
+     echo "$package alredy installed, skipping"
+    fi
+done
