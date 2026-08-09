b8c579a2b35c2d811bad916bdaea15748edd81da
saiteja <saiteja093@gmail.com>
2026-08-09 09:30:45 +0530

    loops ex

---
+++ 14-loops.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+userid=$(id -u)
+logs_folder="/var/log/shell"
+logs_file="/var/log/shell/$0.log"
+
+if [ $userid -ne 0 ]; then
+   echo "please run this script with root user access" | tee -a $logs_file
+   exit 1
+fi  
+mkdir -p $logs_folder
+#### by default shell will not execute, only exectue when it will call
+validate()
+{
+    if [ $1 -ne 0 ]; then
+       echo "$2 ..... failure" | tee -a $logs_file
+       exit 1
+   else
+       echo "$2 ......success" | tee -a $logs_file
+    fi        
+}
+   
+for pacakage in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+  dnf install $pacakage -y &>> $logs_file
+  validate $? "pacakage installation"
+
+done
